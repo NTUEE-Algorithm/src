b8f5480c8e82c8c6b63ae43d42426764ea30fd43
yenlee314159 <b01901053@ntu.edu.tw>
2015-06-24 21:34:56 +0800

    Update graph.h

--- graph.h
+++ graph.h
@@ -18,75 +18,75 @@ class Group;
 class Edge{
 
 	public:
-		Edge(Node *a, Node *b);
-		Node* node[2];
+                Edge(Node *a, Node *b);
+                Node* node[2];
 		
-		Node* getNeighbor(Node *n);
-		bool operator < (const Edge& rhs) const;
+                Node* getNeighbor(Node *n);
+                bool operator < (const Edge& rhs) const;
 };
 
 class Node{
 
 	public:
-		Node(const int i, int* c);
-		void addEdge(Edge *e);
-		void sortEdge();
+                Node(const int i, int* c);
+                void addEdge(Edge *e);
+                void sortEdge();
 		
-		bool isGref();
-		void setToGref();
-		void setGref();
-		bool isGref2();
-		void setToGref2();
-		void setGref2();
+                bool isGref();
+                void setToGref();
+                void setGref();
+                bool isGref2();
+                void setToGref2();
+                void setGref2();
 
-		int id;
-      	int color;
-		int x1;
-		int y1;
-		int x2;
-		int y2;
-		vector<Edge *> edge;
+                int id;
+                int color;
+                int x1;
+                int y1;
+                int x2;
+                int y2;
+                vector<Edge *> edge;
 		
-		int ref;
-		static int gref;
-		int ref2;
-		static int gref2;
+                int ref;
+                static int gref;
+                int ref2;
+                static int gref2;
 		
-		Group* group;
+                Group* group;
 };
 
 class Graph{
 
 	public:
-		Graph() {}
-		~Graph();
+                Graph() {}
+                ~Graph();
 		
-		void addEdge(Node* v1, Node* v2);
-		void sortEdgesOfNode();
-        void sortNodesByID();
-        void sortNodesByX1(); 
-        void sortNodesByY1();  
-        void addEdgesInY();
-        void addEdgesInX();                 
-		void init();
-		Node* getNodeById(const int& id);
+                void addEdge(Node* v1, Node* v2);
+                void sortEdgesOfNode();
+                void sortNodesByID();
+                void sortNodesByX1(); 
+                void sortNodesByY1();  
+                void addEdgesInY();
+                void addEdgesInX();                 
+                void init();
+                Node* getNodeById(const int& id);
 		
-		//use in Map::makeGroup()
-		size_t getNumofNode();
-		bool coloring(Node* h, int* pos);
-		void markAll(Node* h);
-		void setGroup(Node* h, Group* g);
+                //use in Map::makeGroup()
+                size_t getNumofNode();
+                void coloring(Node* h, int* pos, vector<Node*>& nodes, bool& valid)
+                void markAll(Node* h);
+                void setGroup(Node* h, Group* g);
 
-		int alpha;
-		int beta;
-		int omega;
-		int x1;
-		int y1;
-		int x2;
-		int y2;
-		map<int, Node *> nodesMap;
-        vector<Node *> nodes;
-		vector<Edge *> edges;
+                int alpha;
+                int beta;
+                int omega;
+                int x1;
+                int y1;
+                int x2;
+                int y2;
+                map<int, Node *> nodesMap;
+                vector<Node *> nodes;
+                vector<Edge *> edges;
 };
 
 #endif
